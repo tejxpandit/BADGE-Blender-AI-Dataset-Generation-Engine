b25e930fe2970b9415f8e0fed466d146cc1d089d
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-02-18 20:03:33 -0600

    Assign Pink Color to Material
    Defining and assigning the color pink to specific materials in blender.

--- HighlightObject.py
+++ HighlightObject.py
@@ -9,3 +9,10 @@ import bpy
 selected_objs = bpy.context.selected_objects
 non_selected_objs = [obj for obj in bpy.context.scene.objects if obj not in selected_objs]
 
+# Define pink color
+pink = (1, 0.75, 0.75, 1)  # (red, green, blue, alpha)
+
+# Create a new pink material (optional)
+pink_material = bpy.data.materials.new(name="Pink Material")
+pink_material.diffuse_color = pink
+
